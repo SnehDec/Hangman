7a7ce155b446dc90de3d55818b469fcca4b12f09
Sneha Sridhar <snehasgithub@gmail.com>
2022-01-07 14:45:55 -0500

    Added comments, some refactoring and improvements on code.

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express();
 const body_parser = require("body-parser");
 app.use(body_parser.urlencoded({extended:true}));
 let words=[];
-let used_words = [];
+let used_words=[];
 let letters=[];
 let word;
 let def;
@@ -19,7 +19,7 @@ let shortdef=[];
 let categories =["Synonym:", "Antonym:", "Definition:"]
 let func = 0;
 
-
+//the following function is used to get hints for a given word
 function get_info(){
   let cat = Math.floor(Math.random()*3);
   if(number_of_hints<5){
@@ -27,6 +27,7 @@ function get_info(){
     case 0 : if(synonyms){def=synonyms[number_of_hints];}
     else{
       def = antonyms[number_of_hints];
+      //cat has to change to 1 so that the system "knows" that the hint is an antonym now
       cat=1;
     }
     break;
@@ -46,6 +47,7 @@ function get_info(){
     }
     break;
   }
+  //rarely, def may still be undefined because of problems with getting information through the API
   if(def!=undefined){
     number_of_hints+=1;
       hints.push(categories[cat]+def);
@@ -53,7 +55,11 @@ function get_info(){
 }
 }
 
+
+//this function was contributed by Dan Dascalescu and CamHart on StackOverFlow and was modified by me.
+//url to original code:https://stackoverflow.com/questions/6156501/read-a-file-one-line-at-a-time-in-node-js
 async function processLineByLine() {
+  //list_of_words is a text file that contains a list of over 5000 words
   const fileStream = fs.createReadStream(__dirname+'/list_of_words.txt');
 
   const rl = readline.createInterface({
@@ -63,17 +69,50 @@ async function processLineByLine() {
   // Note: we use the crlfDelay option to recognize all instances of CR LF
   // ('\r\n') in input.txt as a single line break.
   for await (const line of rl) {
-    words.push(line);
+    words.push(line); // loads all the words from the database into words array
   }
-  word = words[Math.floor(Math.random()*words.length)];
+  //the code below makes sure that the same word isn't picked twice in a single game.
+  //This is a low probability given the size of the database of words,
+  //but I account for it nonetheless.
+  let word_not_used = false;
+  while(word_not_used==false){
+  word_not_used=true;
+  let index = Math.floor(Math.random()*words.length);
+  for(let i=0;i<used_words.length;i++){
+    if(words[index]==used_words[i]){
+      word_not_used=false;
+    }
+  }
+  word = words[index]; // picks a random word from words array
+}
   used_words.push(word);
-  const letters_shown = word.length-4;
-  let shown_letter = [];
-  for(let i=0; i<letters_shown;i++){
-    shown_letter.push(Math.floor(Math.random()*word.length));
+  const letters_shown = word.length-6; //  6 letters will be hidden from the player
+  let shown_letter = []; // this array will contain the indices of the letters
+  // in word that are to be shown to the user from the beginning. It is populated
+  //by the for loop below.
+  while(shown_letter.length<letters_shown){
+    let push = true;
+    // letter indices will be picked randomly and pushed into shown_letter
+    let letr = Math.floor(Math.random()*word.length);
+    //This for loop prevents duplicates to make sure only 6 words are hidden
+    for(let i=0; i<shown_letter.length;i++){
+      if(letr==shown_letter[i]){
+        push=false;
+      }
+    }
+    if(push=true){
+    shown_letter.push(letr);
   }
+  }
+
+
+//we're sorting shown_letter so that we don't need to use two for loops on line 104.
+  shown_letter.sort(function(a,b){return a-b});
+
   let num=0;
+  //letters contains all letters in the given word
   letters=[];
+  //shown_letters contains the letters that the user sees (including blanks)
   shown_letters = [];
   for(let i=0; i<word.length;i++){
    if(i==shown_letter[num]){
@@ -105,15 +144,20 @@ https.get(options,function(response){
     antonyms = JSON.parse(body)[0].meta.ants[0];
     shortdef = JSON.parse(body)[0].shortdef;
   }
+  //sometimes this specific API has trouble loading information on antonyms
+  //and shortdefs, so we need a backup
   catch{
     synonyms = JSON.parse(body)[0];
   }
     console.log(synonyms);
     console.log(antonyms);
     console.log(shortdef);
+    //We loop through get_info a large number of times to ensure that we get a
+    //minimum number of hints (given the API's occasional issues)
     for(let i=0; i<40;i++){
     get_info();
   }
+  //debugging statements
   console.log("Hints " +hints);
   console.log(letters);
   });
@@ -134,6 +178,8 @@ app.get("/", function(req, res){
     'v', 'w', 'x', 'y', 'z'], letters:letters, hints:hints, func:func}));
 })
 
+//the code below is used to communicate the value of global variable func between server side and client side
+//this allows the Hanged Man to persist even when the page is reloaded
 app.post("/", function(req, res){
  func = Number(req.body.updated_func);
  res.redirect("/");
--- public/hangman_animation.js
+++ public/hangman_animation.js
@@ -1,33 +1,7 @@
-
+//This file handles the Hangman animation and rendering of the hints
 
 let can = document.querySelector(".hangman-canvas");
 let ctx = can.getContext("2d");
-
-let word_letters = document.querySelectorAll(".hidden_word");
-
-let word_letter = document.querySelectorAll("h1");
-for (let i=0; i<word_letter.length; i++){
-  word_letter[i].id = word_letters[i].innerHTML;
-}
-
-
-function allowDrop(ev) {
-  ev.preventDefault();
-}
-
-function drop(ev){
-  ev.preventDefault();
-  if(ev.target.id ==ev.dataTransfer.getData("text")){
-    ev.target.innerHTML = ev.target.id;
-  }
-  else{
-    setTimeout(functions[func], 10).then(func++);
-  }
-}
-
-function drag(ev) {
-  ev.dataTransfer.setData("text", ev.target.innerHTML);
-}
 
 function draw_noose(){
   ctx.moveTo(0, 0);
@@ -75,23 +49,71 @@ function draw_right_leg(){
   ctx.stroke();
 }
 
+//the value of func is grabbed from the server, which renders it as a hidden value
+//on the webpage
 let func = parseInt($("#func")[0].innerHTML);
 
 
 let functions = [draw_noose, draw_head, draw_torso,
   draw_left_arm, draw_right_arm, draw_left_leg, draw_right_leg];
 
-let requested_hints = 0;
-let hints = $(".hidden_hint");
+//The following code enables the Hanged Man to be persistent when the page reloads
+//to generate a new word.
+  if(func>0){
+    for(let i=0; i<func;i++){
+      setTimeout(functions[i], 1);
+    }
+  }
 
-if(func>0){
-  for(let i=0; i<func;i++){
-    setTimeout(functions[i], 1);
+//word_letters contain all the letters of the word
+let word_letters = document.querySelectorAll(".hidden_word");
+
+//word_letter contains the letters that are shown to the user as well as the blanks
+let word_letter = document.querySelectorAll("h1");
+for (let i=0; i<word_letter.length; i++){
+  //Each blank is assigned an id that corresponds
+  // to the letter that is supposed to be filled into that blank
+  word_letter[i].id = word_letters[i].innerHTML;
+}
+
+//The following code handles the drag-and-drop functionality
+function allowDrop(ev) {
+  ev.preventDefault();
+}
+
+function drag(ev) {
+  ev.dataTransfer.setData("text", ev.target.innerHTML);
+}
+
+function drop(ev){
+  ev.preventDefault();
+  //if the letter dragged by the user into a blank is the same as the id
+  //assigned to that blank, the blank gets filled in.
+  //Otherwise, the next part of the Hanged Man is drawn by updating
+  //global variable func.
+  if(ev.target.id ==ev.dataTransfer.getData("text")){
+    ev.target.innerHTML = ev.target.id;
+  }
+  else{
+    setTimeout(functions[func], 10).then(func++);
   }
 }
 
+//requested_hints keeps track of how many times the user hits the Hint button
+let requested_hints = 0;
+//The hints are all rendered as hidden synonyms/antonyms/definitions
+// on the webpage by the server,
+//to be revealed by pressing a button.
+let hints = $(".hidden_hint");
+
+//The following code reveals hints when the Hint button is pressed.
 $("#generate_hint").click(function(){
+  //If the user exceeds the number of available hints (usually 5), one blank gets
+  //filled in each time the user hits the Hint button, with a penalty of proceeding
+  // with the Hanging of the Man
   if(hints.length-1<requested_hints){
+    //if the entire word has been exposed already, this button stops working.
+    //This prevents the while loop on line 129 from going on indefinitely.
     let word_exposed=true;
     for(let i=0; i<word_letter.length;i++){
       if(word_letter[i].innerHTML != word_letter[i].id){
@@ -101,6 +123,8 @@ $("#generate_hint").click(function(){
     if(word_exposed==true){
       this.off("click");
     }
+    //The code below picks a random unfilled blank to fill in, and proceeds
+    //to the next step of hanging the man.
     let i = Math.floor(Math.random()*word_letter.length);
     while(word_letter[i].innerHTML==word_letter[i].id){
       i = Math.floor(Math.random()*word_letter.length);
@@ -108,13 +132,21 @@ $("#generate_hint").click(function(){
     word_letter[i].innerHTML = word_letter[i].id;
     setTimeout(functions[func], 10).then(func++);
   }
+  //if the number of hints is less than 5/the number of hints available on the page,
+  //the hidden hints get revealed. There is no penalty for these hints.
   else{
   let hint = hints[requested_hints].classList.remove("hidden_hint");
 }
   requested_hints+=1;
 })
 
-
+//The timer below checks whether the user has finished filling in the given word.
+//If yes, the program reveals a hidden button on the screen that the user can
+//click to move on to the next word. Thus button is actually a form submit
+//button that simultaneously sends the value of func back to the server, which
+//again renders it on the web page so that the Hanged Man persists upon reloading the page.
+//If the man is completely hanged (func=functions.length), the game ends and the timer
+//stops. 
 let timer = setInterval(function(){  let next_word = true;
   for(let i=0; i<word_letter.length;i++){
     if(word_letter[i].innerHTML!=word_letter[i].id){
